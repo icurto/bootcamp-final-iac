33ad3f3eee624c3720f87767b39452610af51669
Iñigo Curto Alvarez <icurto@acciona.como>
2025-03-18 15:49:27 +0100

    Initial commit

---
+++ terraform/main.tf
@@ -0,0 +1,32 @@
+provider "azurerm" {
+  features {}
+
+  subscription_id = "1ca8d3ea-3b84-49f5-afde-7b4ebe3a62eb"
+}
+
+resource "azurerm_resource_group" "myRg" {
+  name = "ironhack-final-rg"
+
+  location = "West Europe"
+
+}
+
+resource "azurerm_kubernetes_cluster" "default" {
+  name                = "ironhack-final-aks"
+  location            = azurerm_resource_group.myRg.location
+  resource_group_name = azurerm_resource_group.myRg.name
+  dns_prefix          = "ironhack-final-dns-k8s"
+  kubernetes_version  = "1.32.0"
+
+  default_node_pool {
+    name            = "default"
+    node_count      = 2
+    vm_size         = "Standard_D2_v2"
+    os_disk_size_gb = 30
+  }
+
+  identity {
+    type = "SystemAssigned"
+  }
+
+}
