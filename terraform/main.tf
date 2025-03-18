1017116170d77bc919b17e3f1c8cc8e12277eea9
Iñigo Curto Alvarez <icurto@acciona.como>
2025-03-18 17:05:14 +0100

    Añadido storage account para el tfstate

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,3 +1,12 @@
+terraform {
+  backend "azurerm" {
+    resource_group_name  = "ironhack-final-tfstate-rg"
+    storage_account_name = "ironhackfinaltfstatestg"
+    container_name       = "tfstate"
+    key                  = "terraform.tfstate"
+  }
+}
+
 provider "azurerm" {
   features {}
 
